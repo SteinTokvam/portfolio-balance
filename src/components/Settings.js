ad556b79b00d671a629d18992d4aa8d27fde2731
Stein Petter Tokvam <stto1210@gmail.com>
2023-09-25 09:28:38 +0200

    removed file upload ui

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,14 +1,21 @@
 import { Button, Divider, Link, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react"
 import { useDispatch, useSelector } from "react-redux"
 import { addNewInvestment, deleteInvestments, importInvestments } from "../actions/investments"
+import { useRef } from "react"
 
 export default function Settings({ isOpen, onOpenChange }) {
     const dispatch = useDispatch()
     const investments = useSelector(state => state.rootReducer.investments.investments)
 
-    const handleChange = e => {
+    const hiddenFileInput = useRef(null);
+
+    const handleClick = event => {
+        hiddenFileInput.current.click();
+      };
+
+    const importInvestmentsFile = e => {
         const fileReader = new FileReader();
-        fileReader.readAsText(e.target.files[0], "UTF-8");
+        fileReader.readAsText(e.currentTarget.files[0], "UTF-8");
         fileReader.onload = e => {
             console.log("e.target.result", e.target.result);
             dispatch(importInvestments(JSON.parse(e.target.result)));
@@ -22,8 +29,8 @@ export default function Settings({ isOpen, onOpenChange }) {
                     <>
                         <ModalHeader className="flex flex-col gap-1">Settings</ModalHeader>
                         <ModalBody>
-                            <Button color="primary" variant="flat">
-                                <input type="file" onChange={handleChange} />
+                            <Button color="primary" variant="flat" onPress={handleClick}>
+                                <input type="file" className="hidden" onChange={importInvestmentsFile} ref={hiddenFileInput}/>
                                 Import data
                             </Button>
                             <Button color="primary" variant="flat">
