d0043617bb6466fd4fc0b5406a64aaca5e62cb76
Stein Petter Tokvam <stto1210@gmail.com>
2024-03-14 22:38:27 +0100

    dropdown in menu

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,66 +4,22 @@ import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux"
 import NewInvestment from "./NewInvestment";
 import { v4 as uuidv4 } from 'uuid';
+import AccountsTable from "./AccountsTable";
 
 
 export default function Dashboard() {
 
     const { t } = useTranslation();
-    const investments = useSelector(state => state.rootReducer.investments.investments)
-    const accountTypes = useSelector(state => state.rootReducer.accounts.accountTypes)
-
-    const [biggestInvestment, setBiggestInvestment] = useState({})
-
-    useEffect(() => {
-        const res = Math.max.apply(Math, investments.map(function (i) { return i.value; }))
-
-        const foundInvestment = investments.find(function (i) { return i.value === res; })
-        if (foundInvestment !== undefined || foundInvestment !== Infinity) {
-            setBiggestInvestment(foundInvestment)
-        }
-    }, [setBiggestInvestment, investments])
-
-    const totalValueByType = accountTypes.map(accountType => {
-        return { accountType: accountType.name, value: investments.filter(investment => investment.type === accountType.key).reduce((sum, investment) => sum + investment.value, 0) }
-    })
-    const totalValue = investments.reduce((sum, investment) => sum + investment.value, 0);
-
+    
     return (
         <>
             <div className="flex flex-col items-center justify-center">
                 <Spacer y={10} />
-                <h1 className="text-medium text-left font-semibold leading-none text-default-600">{t('dashboard.total')}</h1>
-                <Spacer y={2} />
-                <h2 className="text-large text-left font-bold leading-none text-default-400">{totalValue} {t('valuators.currency')}</h2>
-                <Spacer y={20} />
-            </div >
-            <div className="w-full text-center flex flex-col justify-center">
-                <div className="grid grid-cols-2 gap-20 justify-between">
-                    {
-                        totalValueByType.map(type => {
-                            return (
-                                <div key={type + uuidv4()}>
-                                    <h2 className="text-medium font-semibold leading-none text-default-600">{type.accountType}</h2>
-                                    <Spacer y={2} />
-                                    <h4 className="text-large font-bold leading-none text-default-400">{type.value} {t('valuators.currency')}</h4>
-                                </div>)
-                        })
-                    }
+                <div>
+                    <h1 className="text-large font-semibold">Avkastning</h1>
+                    Her må jeg vise en graf ellerno med avkastning for investeringer
                 </div>
-            </div>
-            {
-                biggestInvestment !== null && biggestInvestment !== undefined && Object.keys(biggestInvestment).length > 0 ?
-                <div className="full text-center mx-auto flex flex-col justify-center">
-                    <Spacer y={20} />
-                    <h1 className="text-medium font-semibold leading-none text-default-600">{t('dashboard.biggestInvestment')}</h1>
-                    <Spacer y={2} />
-                    <h2 className="text-large font-bold leading-none text-default-400">{biggestInvestment.name + ": " + (biggestInvestment.value / totalValue.toFixed(2) * 100).toFixed(2) + t('valuators.percentage')}</h2>
-                    <Spacer y={4} />
-                </div> : ""
-            }
-            <div className='w-full mx-auto text-center'>
-                <NewInvestment />
-            </div>
+            </div >
         </>
     )
 }
--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,19 +1,22 @@
-import { Navbar, NavbarBrand, NavbarContent, useDisclosure, NavbarMenu, NavbarMenuToggle, Link, NavbarMenuItem, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, useDisclosure, NavbarMenu, NavbarMenuToggle, Link, NavbarMenuItem, Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import { Logo } from "../icons/Logo.jsx";
 import Settings from "./Settings.js";
 import { useTranslation } from "react-i18next";
 import { useReducer } from "react";
 import { routes } from "../Util/Global.js";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function MyNavbar() {
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [isMenuOpen, setIsMenuOpen] = useReducer((current) => !current, false);;
+  const accounts = useSelector(state => state.rootReducer.accounts.accountTypes);
   const { t } = useTranslation();
 
   const menuItems = [
     { name: t('navbar.dashboard'), link: routes.dashboard },
+    { name: t('navbar.accounts'), link: "#" },
     { name: t('navbar.portfolio'), link: routes.portfolio },
     { name: t('navbar.rebalancing'), link: routes.rebalancing },
     { name: t('navbar.settings'), link: onOpen },
@@ -26,6 +29,25 @@ export default function MyNavbar() {
     if (isHamburgerMenu) {
       setIsMenuOpen()
     }
+  }
+
+  function generateDropdown(item) {
+    return <Dropdown >
+      <DropdownTrigger>
+        <Link
+          className="w-full"
+          variant="light"
+        >
+          {item.name}
+        </Link>
+      </DropdownTrigger>
+      <DropdownMenu variant="light" aria-label="accountsMenu" onAction={(key) => alert(key)}>
+        <DropdownItem onClick={() => handleClick(isMenuOpen, item.link)}>{t('navbar.allAccounts')}</DropdownItem>
+        {accounts.map(account => {
+          return <DropdownItem key={account.key}>{account.name}</DropdownItem>
+        })}
+      </DropdownMenu>
+    </Dropdown>
   }
 
   return (
@@ -43,16 +65,24 @@ export default function MyNavbar() {
 
       <NavbarContent justify="end" className="hidden sm:flex lg:flex">
         <Settings isOpen={isOpen} onOpenChange={onOpenChange} />
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item.name}-${index}`}>
-            <Button variant="light"
-              className="w-full"
-              onPress={() => item.name === t('navbar.settings') ? handleClick(false, onOpen) : handleClick(false, () => navigate(item.link))}
-            >
-              {item.name}
-            </Button>
-          </NavbarMenuItem>
-        ))}
+        {menuItems.map((item, index) => {
+          if (item.link === "#") {
+            return (
+              generateDropdown(item)
+            )
+          }
+          return (
+            <NavbarMenuItem key={`${item.name}-${index}`}>
+              <Link variant="light"
+                className="w-full"
+                onPress={() => item.name === t('navbar.settings') ? handleClick(false, onOpen) : handleClick(false, () => navigate(item.link))}
+              >
+                {item.name}
+              </Link>
+            </NavbarMenuItem>
+          )
+        })
+        }
       </NavbarContent>
 
       <NavbarContent justify="end">
@@ -66,17 +96,38 @@ export default function MyNavbar() {
       </NavbarContent>
 
       <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item.name}-${index}`}>
-            <Link
-              className="w-full"
-              onPress={() => item.name === t('navbar.settings') ? handleClick(true, onOpen) : handleClick(true, () => navigate(item.link))}
-              size="lg"
-            >
-              {item.name}
-            </Link>
-          </NavbarMenuItem>
-        ))}
+        {menuItems.map((item, index) => {
+          if (item.link === "#") {
+            if (item.link === "#") {
+              return (
+                <>
+                  <Link className="w-full" onPress={() => console.alert("Open all accounts")}>{item.name}</Link>
+                  {accounts.map(account => {
+                    return (
+                      <Link
+                        className="w-full pl-5"
+                        onPress={() => item.name === t('navbar.settings') ? handleClick(true, onOpen) : handleClick(true, () => navigate(item.link))}
+                        size="lg"
+                      >
+                        {account.name}
+                      </Link>)
+                  })}
+                </>
+              )
+            }
+          }
+          return (
+            <NavbarMenuItem key={`${item.name}-${index}`}>
+              <Link
+                className="w-full"
+                onPress={() => item.name === t('navbar.settings') ? handleClick(true, onOpen) : handleClick(true, () => navigate(item.link))}
+                size="lg"
+              >
+                {item.name}
+              </Link>
+            </NavbarMenuItem>
+          )
+        })}
       </NavbarMenu>
     </Navbar>
   )
