30cef57aa9e57e92c45e6e3e738bb075246962dd
Stein Petter Tokvam <stto1210@gmail.com>
2024-03-22 14:14:13 +0100

    remove old account components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,6 @@ import { Spacer } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux"
-import NewInvestment from "./NewInvestment";
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Dashboard() {
