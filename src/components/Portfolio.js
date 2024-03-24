4731268cb7e38f2b09f9227d2a2ee0beb787bf80
Stein Petter Tokvam <stto1210@gmail.com>
2024-03-24 12:51:58 +0100

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -94,6 +94,7 @@ export default function Portfolio() {
                                                                                     </div>
                                                                                 )
                                                                             }
+                                                                            return <></>
                                                                         })
                                                                     }
                                                                 </div>
