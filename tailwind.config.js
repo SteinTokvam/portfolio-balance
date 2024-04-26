64ae45944cb72e027a681b352fbd17094620ce34
Stein Petter Tokvam <stto1210@gmail.com>
2024-04-26 14:44:12 +0200

    tailwind ts config test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 import { nextui } from "@nextui-org/react";
-export const content = ["./src/**/*.{html,js}",
+export const content = ["./src/**/*.{html,js,jsx,ts,tsx}",
   "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",];
 export const theme = {
   extend: {},
