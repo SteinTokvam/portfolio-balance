051b8efb691fb55952ea2db931bf556c42f6472b
Stein Petter Tokvam <stto1210@gmail.com>
2024-08-28 08:55:42 +0200

    Update BareBitcoin.ts

--- src/Util/BareBitcoin.ts
+++ src/Util/BareBitcoin.ts
@@ -1,18 +1,22 @@
 import { Account, Holding, Transaction } from "../types/Types";
 
-const isLocal = false
+const isLocal = true
 const baseUrl = isLocal ? 'http://localhost:3000' : 'https://portfolio-balance-backend.onrender.com'
 
 export async function fetchBBTransactions(account: Account): Promise<Transaction[]> {
     try {
         const response = await fetch(`${baseUrl}/barebitcoin/transactions`, {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
-                'accessKey': account.apiInfo?.accessKey
+                'accessKey': account.apiInfo ? account.apiInfo.accessKey : '',
             })
         })
     
         const res = await response.json()
+        console.log('Fetched Bare bitcoin transactions', res)
         return res
     } catch(error){
         console.log(error)
@@ -24,13 +28,17 @@ export async function fetchBBHoldings(account: Account): Promise<Holding[]> {
     try {
         const response = await fetch(`${baseUrl}/barebitcoin/balance`, {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
-                'accessKey': account.apiInfo?.accessKey,
+                'accessKey': account.apiInfo ? account.apiInfo.accessKey : '',
                 'accountKey': account.key
             })
         })
     
         const res = await response.json()
+        console.log('Fetched Bare Bitcoin holdings', res)
         return res
     } catch(error){
         console.log(error)
